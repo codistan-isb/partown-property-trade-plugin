2c5289c6502ceee0389d8272484f4adac0c34b8c
muhammadirtaza <muhammad.irtaza@codistan.org>
2023-04-29 01:54:16 +0500

    fix: wrong subscription account id

--- src/resolver/Subscription.js
+++ src/resolver/Subscription.js
@@ -2,12 +2,12 @@ export default {
   notifications: {
     subscribe: function subscribe(parent, args, context, info) {
       console.log("notifications subscription function");
-      let { userId } = args;
+      let { accountId } = args;
       let { pubSub } = context;
 
-      console.log("pubsub is ", pubSub);
+      console.log("pubsub is ", accountId);
 
-      return pubSub.asyncIterator(`notifications-${userId}`);
+      return pubSub.asyncIterator(`notifications-${accountId}`);
     },
   },
 };
